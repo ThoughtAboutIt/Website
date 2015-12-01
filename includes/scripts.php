7d1fd57a5550602e30a20b9ff58f58b1cc9b8e6a
ColossalTr1gg3r <simonthow1996@gmail.com>
2015-12-01 13:26:43 +0000

    Create scripts.php

---
+++ includes/scripts.php
@@ -0,0 +1,11 @@
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/index.js"></script>
+<script>
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+</script>
