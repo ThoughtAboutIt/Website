e0bee356c2e0306422918f704e18d67fb85494b8
ColossalTr1gg3r <simonthow1996@gmail.com>
2015-12-01 13:31:58 +0000

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,46 @@
+<!-- Session -->
+<?php include_once "includes/session.php";// include for session ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard</title>
+  <!-- Bootstrap Core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="css/simple-sidebar.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+
+<body>
+
+<div id="wrapper">
+<!-- Navigation bar -->
+<?php include_once "includes/nav.php";// include for nav ?>
+    </div>
+<!-- Page Content -->
+<div id="page-content-wrapper">
+    <nav class="top-nav"> 
+        <div class="container">   
+          <center><div class="nav-wrapper"><h1>Dashboard</h1></div></center>
+        </div>
+    </nav>
+        <div class="container-fluid">
+            <div class="row">
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+<!-- Footer -->
+<?php include_once "includes/footer.php";// include for Footer ?>     
+<!-- JS -->
+<?php include_once "includes/scripts.php";// include for Javascripts ?>
+
+</body>
+
+</html>
