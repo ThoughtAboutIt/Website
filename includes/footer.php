4de6dc7313c0882355edc1fb8239615813eedaae
ColossalTr1gg3r <simonthow1996@gmail.com>
2015-12-01 13:27:01 +0000

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="black-text">Company Bio</h5>
+            <p class="black-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed                                   sit amet venenatis mauris. Cum sociis natoque penatibus et                                     magnis dis parturient montes, nascetur ridiculus mus.                                           Pellentesque eu velit ut tortor pharetra tincidunt nec ut                                       tellus. Sed elementum euismod magna, quis cursus quam                                           porttitor sed.
+            </p>
+          </div>
+    <div class="col l3 s12">
+        <h5 class="black-text">Links</h5>
+            <ul>
+            <li><a class="black-text" href="index.html">Home</a></li>
+            <li><a class="black-text" href="store.html">Store</a></li>
+            <li><a class="black-text" href="about.html">About</a></li>
+            <li><a class="black-text" href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      <a>ThoughtAboutIt &copy; 2015</a>
+      </div>
+      </div>
+    </footer>
+    </div>
